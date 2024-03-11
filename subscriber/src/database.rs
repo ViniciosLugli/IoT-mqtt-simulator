d4fe451566c32ab25abb679925655dc6e69bb925
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-11 20:29:53 -0300

    feat(subscriber): :sparkles: Setup prisma env

---
+++ subscriber/src/database.rs
@@ -0,0 +1,5 @@
+#[allow(warnings, unused)]
+mod prisma;
+
+use prisma::PrismaClient;
+use prisma_client_rust::NewClientError;
--- subscriber/src/main.rs
+++ subscriber/src/main.rs
@@ -1,6 +1,6 @@
+mod database;
 use common::mqtt::MqttClient;
 use std::{error::Error, time::Duration};
-
 fn main() -> Result<(), Box<dyn Error>> {
 	let mut mqtt_client = MqttClient::new("SUBSCRIBER")?;
 	mqtt_client.connect(Duration::from_secs(60), true).unwrap();
