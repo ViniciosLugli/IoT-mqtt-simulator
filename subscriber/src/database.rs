e5dec0c394c5f623a5075479996272d2243a1b10
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-11 21:14:29 -0300

    feat(subscriber): :sparkles: Added integration with database on prisma-rs

--- subscriber/src/database.rs
+++ subscriber/src/database.rs
@@ -1,5 +1,39 @@
-#[allow(warnings, unused)]
-mod prisma;
+use serde::{Deserialize, Deserializer};
 
-use prisma::PrismaClient;
-use prisma_client_rust::NewClientError;
+#[allow(warnings, unused)]
+use crate::models::*;
+use std::error::Error;
+
+fn parse_float_from_string<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	let s: String = Deserialize::deserialize(deserializer)?;
+	s.parse::<f64>().map_err(serde::de::Error::custom)
+}
+
+#[derive(Deserialize)]
+pub struct SensorData {
+	sensor: String,
+	#[serde(deserialize_with = "parse_float_from_string")]
+	value: f64,
+	unit: String,
+}
+
+pub struct Database {
+	client: PrismaClient,
+}
+
+impl Database {
+	// As funções permanecem as mesmas, mas agora utilizam a instância concreta.
+	pub async fn new() -> Result<Self, Box<dyn Error>> {
+		let client = PrismaClient::_builder().build().await.unwrap();
+
+		Ok(Self { client })
+	}
+
+	pub async fn add_sensor_reading(&self, data: SensorData) -> Result<(), Box<dyn Error>> {
+		self.client.sensor_reading().create(data.sensor, data.value, data.unit, vec![]).exec().await.unwrap();
+		Ok(())
+	}
+}
--- subscriber/src/main.rs
+++ subscriber/src/main.rs
@@ -1,15 +1,37 @@
 mod database;
+
+#[allow(warnings, unused)]
+pub mod models;
+
 use common::mqtt::MqttClient;
+use database::{Database, SensorData};
 use std::{error::Error, time::Duration};
-fn main() -> Result<(), Box<dyn Error>> {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
 	let mut mqtt_client = MqttClient::new("SUBSCRIBER")?;
-	mqtt_client.connect(Duration::from_secs(60), true).unwrap();
+	mqtt_client.connect(Duration::from_secs(60), true)?;
 
 	mqtt_client.subscribe("sensors", 1)?;
 	let rx = mqtt_client.start_consuming();
+
+	let db = Database::new().await?;
+
 	for msg in rx.iter() {
 		if let Some(msg) = msg {
 			println!("{}", msg);
+
+			// Aqui, vamos tentar deserializar a mensagem MQTT em SensorData.
+			match serde_json::from_str::<SensorData>(&msg.payload_str()) {
+				Ok(sensor_data) => {
+					// Se a deserialização for bem-sucedida, insira os dados no banco de dados.
+					match db.add_sensor_reading(sensor_data).await {
+						Ok(_) => println!("Sensor reading added to the database."),
+						Err(e) => eprintln!("Failed to add sensor reading to the database: {}", e),
+					}
+				}
+				Err(e) => eprintln!("Failed to deserialize message: {}", e),
+			}
 		}
 	}
 
